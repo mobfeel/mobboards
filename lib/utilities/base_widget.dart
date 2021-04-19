81e1f21268f9f4677a1b1250a9aa51975c2a36d4
EduardoRambo <eduardorambolima@gmail.com>
2021-04-19 18:30:28 -0300

    -> Changed Import on base_widget.dart

--- lib/utilities/base_widget.dart
+++ lib/utilities/base_widget.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:mobboards/utils/sizing_information.dart';
+import 'package:mobboards/utilities/sizing_information.dart';
 
 class BaseWidget extends StatelessWidget {
   final Widget Function(
